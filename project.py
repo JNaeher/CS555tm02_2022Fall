73226b12ffde7b8cbef872ec8e7ab6aa4dad3334
Jake Naeher <73560158+JNaeher@users.noreply.github.com>
2023-02-08 16:09:01 -0500

    test

--- project.py
+++ project.py
@@ -19,6 +19,34 @@ def exists(elem, list):
 # turns a string into a date object
 def date_format(string):
     temp = string.split(" ", 2)
+    if(len(temp) == 2):
+        month = 0
+        if(temp[1] == 'JAN'):
+            month = 1
+        if(temp[1] == 'FEB'):
+            month = 2
+        if(temp[1] == 'MAR'):
+            month = 3
+        if(temp[1] == 'APR'):
+            month = 4
+        if(temp[1] == 'MAY'):
+            month = 5
+        if(temp[1] == 'JUN'):
+            month = 6
+        if(temp[1] == 'JUL'):
+            month = 7
+        if(temp[1] == 'AUG'):
+            month = 8
+        if(temp[1] == 'SEP'):
+            month = 9
+        if(temp[1] == 'OCT'):
+            month = 10
+        if(temp[1] == 'NOV'):
+            month = 11
+        if(temp[1] == 'DEC'):
+            month = 12
+        year = int(temp[1])
+        return date(year, month)
     day = int(temp[0])
     year = int(temp[2])
     month = 0
