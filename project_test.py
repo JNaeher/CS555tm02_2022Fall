aa490e4dd178db95a31ac1c6be7cba214da285a9
Jake Naeher <73560158+JNaeher@users.noreply.github.com>
2023-02-11 11:30:47 -0500

    one family id test

--- project_test.py
+++ project_test.py
@@ -2,6 +2,11 @@ from project import *
 import unittest
 
 class TestUniqueID(unittest.TestCase):
-    def test_individual(self):
+    def test_individual1(self):
         self.assertEqual(unique_indiv_id('project1.ged'), True)
+
+    def test_individual2(self):
         self.assertEqual(unique_indiv_id('test1.ged'), True)
+
+    def test_family1(self):
+        self.assertEqual(unique_family_id('project1.ged'), True)
