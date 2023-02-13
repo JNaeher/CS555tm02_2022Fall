2df042608d0f63c42998d631fa9d6135d5df5c6a
Jake Naeher <73560158+JNaeher@users.noreply.github.com>
2023-02-13 10:13:18 -0500

    comments in test file

--- project_test.py
+++ project_test.py
@@ -2,6 +2,9 @@ from project import *
 import unittest
 
 class TestUniqueID(unittest.TestCase):
+    # naeher_test1 has unique ids for both individuals and families
+    # naeher_test2 has unique ids for both individuals and families
+    # naeher_test3 has duplicate ids for both inidividuals and families
     def test_individual1(self):
         self.assertEqual(unique_indiv_id('naeher_test1.ged'), True)
 
